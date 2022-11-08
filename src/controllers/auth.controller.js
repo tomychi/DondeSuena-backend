e68f6587398737ae20cb36e850e3a08e59d4d00e
tomychi <tomychi352@gmail.com>
2022-11-08 00:39:00 -0300

    ruta /registerUser creada

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,49 @@
 const { response } = require('express');
-const User = require('../models/User');
+const { User } = require('../db');
+const bcrypt = require('bcryptjs');
 
-const createUser = async (req, res = response) => {};
+const createUser = async (req, res = response) => {
+    const { email, password, password2 } = req.body;
+
+    try {
+        let user = await User.findOne({ where: { email } });
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese email',
+            });
+        }
+
+        if (password !== password2) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Las contraseñas no coinciden',
+            });
+        }
+        user = new User(req.body);
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt); // me genera un hash
+
+        await user.save();
+
+        res.status(201).json({
+            ok: true,
+            msg: 'Usuario creado',
+            uid: user.id,
+            name: user.name,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador',
+        });
+    }
+};
+
+module.exports = {
+    createUser,
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,20 +1,37 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
 const { createUser } = require('../controllers/auth.controller');
 
 router.post(
-    '/newUser',
+    '/registerUser',
     [
-        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('firstName', 'El nombre es obligatorio').not().isEmpty(),
+        check('lastName', 'El apellido es obligatorio').not().isEmpty(),
         check('email', 'Agrega un email válido').isEmail(),
         check(
             'password',
             'El password debe ser de al menos 6 caracteres'
         ).isLength({ min: 6 }),
+        check('password2', 'El password deben ser iguales').custom(
+            (value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error('Los passwords no son iguales');
+                }
+                return true;
+            }
+        ),
+        check('phone', 'El teléfono es obligatorio').not().isEmpty(),
+        check('birthday', 'La fecha de nacimiento es obligatoria')
+            .not()
+            .isEmpty(),
+
+        validateFields,
     ],
+
     createUser
 );
 
