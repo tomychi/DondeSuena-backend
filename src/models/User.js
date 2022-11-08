4022d02b80761bf899513d61e07815e355641bff
tomychi <tomychi352@gmail.com>
2022-11-08 11:20:03 -0300

    state agregado al User models

--- src/models/User.js
+++ src/models/User.js
@@ -41,5 +41,9 @@ module.exports = (sequelize) => {
             type: DataTypes.BOOLEAN,
             defaultValue: false,
         },
+        state: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+        },
     });
 };
