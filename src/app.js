4168ad85215c4003223af0d123b4fba3a317e130
tomychi <tomychi352@gmail.com>
2022-11-09 19:01:01 -0300

    rutas de places

--- src/app.js
+++ src/app.js
@@ -1,50 +1,52 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 // MODULARIZAR RUTAS
-const authUser = require("./routes/user.routes.js");
-const event = require("./routes/event.routes.js");
+const authUser = require('./routes/user.routes.js');
+const event = require('./routes/event.routes.js');
 const post = require('./routes/posts.routes.js');
 const reaction = require('./routes/reactions.routes.js');
+const place = require('./routes/place.routes.js');
 
-require("./db");
-
-const post = require("./routes/posts.routes.js");
-const reaction = require("./routes/reactions.routes.js");
+require('./db');
 
 const server = express();
-const cors = require("cors");
+const cors = require('cors');
 
-server.name = "API";
+server.name = 'API';
 server.use(cors());
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
 });
 
-server.use("/auth", authUser);
-server.use("/event", event);
-server.use("/post", post);
-server.use("/reaction", reaction);
+server.use('/auth', authUser);
+server.use('/event', event);
+server.use('/post', post);
+server.use('/reaction', reaction);
+server.use('/place', place);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+    // eslint-disable-line no-unused-vars
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
 });
 
 module.exports = server;
