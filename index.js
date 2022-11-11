cd3b5c4e074e15113cc4a83c2144d3d177beb4e1
Lautaro <lautiisantillan8@gmail.com>
2022-11-11 16:54:19 -0300

    updated

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-require('dotenv').config();
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
 const {
-    loadUsers,
-    loadEvents,
-    loadPlaces,
-    loadArtists,
-    loadGenres,
-} = require('./src/controllers/loadsDatabase');
+  loadUsers,
+  loadEvents,
+  loadPlaces,
+  loadArtists,
+  loadGenres,
+} = require("./src/controllers/loadsDatabase");
 
 const port = process.env.PORT || 3000;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
-    loadUsers();
-    loadEvents();
-    loadArtists();
-    loadPlaces();
-    loadGenres();
-    server.listen(port, () => {
-        console.log(`servidor corriendo en puerto: ${port}`); // eslint-disable-line no-console
-    });
+  loadUsers();
+  loadEvents();
+  loadArtists();
+  loadPlaces();
+  loadGenres();
+  server.listen(port, () => {
+    console.log(`servidor corriendo en puerto: ${port}`); // eslint-disable-line no-console
+  });
 });
--- src/models/Event.js
+++ src/models/Event.js
@@ -1,55 +1,55 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define(
-        'event',
-        {
-            id: {
-                type: DataTypes.UUID,
-                defaultValue: DataTypes.UUIDV4,
-                allowNull: false,
-                primaryKey: true,
-            },
-            name: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            description: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            date: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            start: {
-                type: DataTypes.TIME,
-                allowNull: false,
-            },
-            end: {
-                type: DataTypes.TIME,
-                allowNull: false,
-            },
-            price: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            quotas: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            image: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            state: {
-                type: DataTypes.BOOLEAN,
-                defaultValue: true,
-            },
-        },
+  sequelize.define(
+    "event",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      start: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      end: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      quotas: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
 
-        {
-            timestamps: false,
-        }
-    );
+    {
+      timestamps: false,
+    }
+  );
 };
